e8389f5d55f01f4a5d0c213d3fe7fc95b002b4af
IngRamirezJezhuah <ramirezalvaradodavidjezhuah@gmail.com>
2025-06-13 21:27:53 -0600

    update-pantallas

--- FRONTEND/Web/integradora_2/src/Screens/Component.jsx
+++ FRONTEND/Web/integradora_2/src/Screens/Component.jsx
@@ -7,29 +7,28 @@ const Component=() => {
         <div className='contenedor'>
             <div className='tabNavigator'>
                 <div>
-                <img className='menu' src="/menu.png" alt='coso del menu'></img>
-                <Link to="/Dashboard">
-                    <li className="bordes"></li>
-                    <li>Dashboard</li>
-                </Link>
-                <Link to="/Pacientes">
-                    <li className="bordes"></li>
-                    <li>Pacientes</li>
-                </Link>
-                <Link to="/Pedidos">
-                    <li className="bordes"></li>
-                    <li>Pedido</li>
-                </Link>
-                <Link to="/Muestras">
-                    <li className="bordes"></li>
-                    <li>Muestras</li>
-                </Link>
-                <Link to="/Analisis">
-                    <li className="bordes"></li>
-                    <li>analisis</li>
-                </Link>
-                    <div className='cerrar-sesion'></div>
-                    
+                    <img className='menu' src="/menu.png" alt='coso del menu' />
+                    <Link to="/Dashboard">
+                        <li className="bordes"><img src="/dash.png" alt="" /></li>
+                        <li>Dashboard</li>
+                    </Link>
+                    <Link to="/Pacientes">
+                        <li className="bordes"><img src="/usuario.png" alt="" /></li>
+                        <li>Pacientes</li>
+                    </Link>
+                    <Link to="/Pedidos">
+                        <li className="bordes"><img src="/pedido.png" alt="" /></li>
+                        <li>Pedido</li>
+                    </Link>
+                    <Link to="/Muestras">
+                        <li className="bordes"><img src="/mues.png" alt="" /></li>
+                        <li>Muestras</li>
+                    </Link>
+                    <Link to="/Analisis">
+                        <li className="bordes"></li>
+                        <li>analisis</li>
+                    </Link>
+                        <div className='cerrar-sesion'></div>
                 </div>
                 <div className='plantilla'>
                     <Outlet />
--- FRONTEND/Web/integradora_2/src/Screens/Pacientes.jsx
+++ FRONTEND/Web/integradora_2/src/Screens/Pacientes.jsx
@@ -10,10 +10,10 @@ const Pacientes =() => {
             </div>
             <div className='caja_1'>
                 <div className='prueba_tabla'>
-                    <img src="/prueba-de-sangre.png" alt="prueba imagen" className='imagenes' />
+                    <img src="/user-nat.png" alt="prueba imagen" className='imagenes' />
                     <p className='prueba-name'>Mario Lira Zamora</p>
-                    <img src="/borrar.png" alt="editar" className='iconos' />
-                    <img src="/editar.png" alt="borrar" className='iconos' />
+                    <img src="/ajustes-deslizadores.png" alt="editar" className='iconos' />
+                    <img src="/basura.png" alt="borrar" className='iconos' />
                 </div>
                 <hr />
             </div>
@@ -22,9 +22,10 @@ const Pacientes =() => {
                 <div className='tabla-detalles'>
                     <img src="/prueba-de-sangre.png" alt="prueba imagen" className='imagenes' />
                     <p className='prueba-name'>Quimica Sangiunea</p>
-                    <p>(qt)</p>
-                    <img src="/email.png" alt="editar" className='iconos' />
-                    <img src="/descargas.png" alt="borrar" className='iconos' />
+                    <div>
+                        <img src="/email.png" alt="editar" className='iconos' />
+                        <img src="/descargas.png" alt="borrar" className='iconos' />
+                    </div>
                 </div>
                 
             </div>
