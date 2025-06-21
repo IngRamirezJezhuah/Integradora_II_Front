f31d8db3862cb18a94659444b98722261bf3f792
IngRamirezJezhuah <ramirezalvaradodavidjezhuah@gmail.com>
2025-06-21 13:27:50 -0600

    version-movil-update

--- FRONTEND/Movil/integradora/App.js
+++ FRONTEND/Movil/integradora/App.js
@@ -6,14 +6,14 @@ import TabNavigator from './navigation/TabNavigator';
 export const AuthContext = React.createContext(); // Para compartir estado
 
 export default function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false); // Estado global
-  {/*
+  //const [isAuthenticated, setIsAuthenticated] = useState(false);  Estado global
+  /*
       Esto es para cuando se vaya a hacer la autentificacion
       por ahora es inutl asi que no la usaremos
-  <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated }}> 
-  isAuthenticated ? <TabNavigator /> : <AuthStack />
-  </AuthContext.Provider> 
-  */}
+    <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated }}> 
+      isAuthenticated ? <TabNavigator /> : <AuthStack />
+    </AuthContext.Provider> 
+  */
   return (
       <NavigationContainer>
         <TabNavigator />
