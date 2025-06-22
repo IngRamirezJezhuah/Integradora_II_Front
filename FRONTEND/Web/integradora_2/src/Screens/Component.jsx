813119d2dc2d447c76d4619dc66806106ec7b3de
DiegoDaher <diego_3141201023@utd.edu.mx>
2025-06-22 17:21:40 -0600

    barra avance pdt:formalizacion de elementos

--- FRONTEND/Web/integradora_2/src/Screens/Component.jsx
+++ FRONTEND/Web/integradora_2/src/Screens/Component.jsx
@@ -5,29 +5,31 @@ import Dashboard from './Dashboard';
 
 const Component=() => {
     const location = useLocation();
+
+    const isActive = (path) => location.pathname === path;
     return (
         <div className='contenedor'>
             <div className='tabNavigator'>
                 <div>
                     <img className='menu' src="/menu.png" alt='icon-menu' />
                     <Link to="/Dashboard">
-                        <li className="bordes"><img src="/dash.png" alt="" /></li>
+                        <li className={`bordes ${isActive('/Dashboard') ? 'active' : ''}`}><img src="/dash.png" alt="" /></li>
                         <li>Dashboard</li>
                     </Link>
                     <Link to="/Pacientes">
-                        <li className="bordes"><img src="/usuario.png" alt="" /></li>
+                        <li className={`bordes ${isActive('/Pacientes') ? 'active' : ''}`}><img src="/usuario.png" alt="" /></li>
                         <li>Pacientes</li>
                     </Link>
                     <Link to="/Pedidos">
-                        <li className="bordes"><img src="/pedido.png" alt="" /></li>
+                        <li className={`bordes ${isActive('/Pedidos') ? 'active' : ''}`}><img src="/pedido.png" alt="" /></li>
                         <li>Pedido</li>
                     </Link>
                     <Link to="/Muestras">
-                        <li className="bordes"><img src="/muestras.png" alt="" /></li>
+                        <li className={`bordes ${isActive('/Muestras') ? 'active' : ''}`}><img src="/muestras.png" alt="" /></li>
                         <li>Muestras</li>
                     </Link>
                     <Link to="/Analisis">
-                        <li className="bordes"><img src="/analisis.png" alt="" /></li>
+                        <li className={`bordes ${isActive('/Analisis') ? 'active' : ''}`}><img src="/analisis.png" alt="" /></li>
                         <li>analisis</li>
                     </Link>
                     <Link to='/HeaderSlide'>
