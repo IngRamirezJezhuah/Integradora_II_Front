be9427e80ad65e8c827fece247f9d84ecac2ebba
IngRamirezJezhuah <ramirezalvaradodavidjezhuah@gmail.com>
2025-06-22 23:22:44 -0600

    conexion backend

--- FRONTEND/Web/integradora_2/src/Screens/Pacientes.jsx
+++ FRONTEND/Web/integradora_2/src/Screens/Pacientes.jsx
@@ -1,5 +1,5 @@
 //rfce para hacer una plantilla
-import React from 'react'
+//import React, { useEffect, useState } from 'react'
 //import {useState,useEffect} from 'react'
 import { Link } from 'react-router-dom';
 import { SearchBar } from '../Components';
@@ -13,40 +13,31 @@ const Pacientes =() => {
     "Racardo Luna Unzueta",
     "Diego Daher Diaz Contreraz"
     ];
-
+    
     const pruebas = [
         "Quimica sanguinea",
         "Biometrica Hepatica"
     ]
-    /* version con servidor, no funciona de manera local
-    const [pacientes, setPacientes] = useState([])
-    useEffect(() => {
-        const fetchPacientes = async () => {
-            try {
-                const response = await fetch("https://8d5e-189-197-191-34.ngrok-free.app/usuarios", {
-                    method: "GET",
-                    headers: {
-                        "Content-Type": "application/json"
-                    }
-                })
-                const data = await response.json()
-                if (response.ok) {
-                    setPacientes(data) // aquí asumimos que data es un array de pacientes
-                    console.log("Pacientes cargados:", data)
-                } else {
-                    console.error("Error:", data.message)
-                    alert("Error al obtener pacientes")
-                }
-            } catch (error) {
-                console.error("Error en la petición:", error)
-                alert("No se pudo conectar al servidor")
-            }
-        }
+    /*const [pacientes, setPacientes] = useState([]);
+    
 
-        fetchPacientes()
-    }, [])*/
+    useEffect(() => {
+    const getPacientes = async () => {
+        try {
+            const res = await fetch("https://8d5e-189-197-191-34.ngrok-free.app/usuarios");
+            const data = await res.json();
+            console.log("Pacientes desde backend:", data);
+            setPacientes(data);
+        } catch (error){
+            console.error("error al obtener pacientes",error);
+        }
+    };
+    getPacientes();
+}, []);  */
+
     return (
             <div>
+                <h1>Pacientes </h1>
                 <div className='buscador'>
                     <Link to="/Nuevo-Paciente-Form">
                     <button className='btn-agregar'>+Agregar</button>
@@ -57,31 +48,29 @@ const Pacientes =() => {
                     <div className='scale-up-ver-center'>
                             <div className='caja_1'>
                                 <div className='scroll'>
-                                    {/* version con servidor, no funciona en local
-                                        {pacientes.map((paciente, index) => {
-                                            const nombreCompleto = `${paciente.nombre} ${paciente.apellidoPaterno} ${paciente.apellidoMaterno}`;
-                                            const inicial = paciente.nombre.charAt(0);
-                                            return (
-                                                <div key={index} className='prueba_tabla'>
-                                                    <div className='inicial-circulo'>
-                                                        {inicial}
-                                                    </div>
-                                                    <div>
-                                                        <p className='prueba-name'>{nombreCompleto}</p>
-                                                    </div>
-                                                    <div className='acomodar-iconos'>
-                                                        <img src="/basura.png" alt="borrar" className='iconos' />
-                                                    </div>
+                                    {/*
+                                    {pacientes.map((paciente, index) => {
+                                        const nombreCompleto = `${paciente.nombre} ${paciente.apellidoPaterno} ${paciente.apellidoMaterno}`;
+                                        const inicial = paciente.nombre.charAt(0);
+                                        return (
+                                            <div key={index} className='prueba_tabla'>
+                                                <div className='inicial-circulo'>{inicial}</div>
+                                                <div>
+                                                    <p className='prueba-name'>{nombreCompleto}</p>
                                                 </div>
-                                            );
+                                                <div className='acomodar-iconos'>
+                                                    <img src="/basura.png" alt="borrar" className='iconos' />
+                                                </div>
+                                            </div>
+                                        );
                                         })}
                                     */}
-                                    {nombres.map((nombreCompleto,index) => {/*se usa map para iterar la lista y generar un bloque prueba_tabla por cada nombre tambien la usare en chechbox*/
-                                        const inicial = nombreCompleto.charAt(0);/*se obtiene la inicial con .charAt(0) para cada nombre dentro del map. recordar esta mamada*/
+                                    {nombres.map((nombreCompleto,index) => {
+                                        const inicial = nombreCompleto.charAt(0);
                                         return(
-                                            <div key={index} className='prueba_tabla'>{/*cada hijo renderizado con map debe tener una key, por eso usé el índice index. */}
+                                            <div key={index} className='prueba_tabla'>
                                                 <div className='inicial-circulo'>
-                                                    {inicial}
+                                                    <p className='letra-circulo'>{inicial}</p>
                                                 </div>
                                                 <div>
                                                     <p className='prueba-name'>{nombreCompleto}</p>                                
@@ -120,5 +109,8 @@ const Pacientes =() => {
             </div>
     )
 }
-
 export default Pacientes;
+/* 
+en consola da un error para recibir n el back 
+main.ff0e15bcc3380cc32ddd.hot-update.js:49 error al obtener pacientes SyntaxError: Unexpected token '<', "<!DOCTYPE "... is not valid JSON
+*/
