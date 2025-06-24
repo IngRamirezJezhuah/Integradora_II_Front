c0ea37baba3e7ad1c7d0d7ac6e5c6b26f9c9b872
IngRamirezJezhuah <ramirezalvaradodavidjezhuah@gmail.com>
2025-06-24 12:42:21 -0600

    update

--- FRONTEND/Movil/integradora/screens/dashboard.js
+++ FRONTEND/Movil/integradora/screens/dashboard.js
@@ -8,19 +8,19 @@ const dashboard = () => {
     return (
         <View style={Dashboard.container}>
         <Text>Dashboard</Text>
-        <View style={Dashboard.caja_1}>
+        <View style={Dashboard.caja}>
             <Text>Temperatura del Laboratorio </Text>
             <Text style={Dashboard.TextoAlerta}>25°C</Text>
         </View>
-        <View style={Dashboard.caja_1}>
+        <View style={Dashboard.caja}>
             <Text>Humedad del Laboratorio</Text>
             <Text style={Dashboard.TextoAlerta}>10%</Text>
         </View>
-        <View style={Dashboard.caja_2}>
-            <Text>contenedor #id</Text>
+        <View style={Dashboard.caja}>
+            <Text >contenedor #id</Text>
             <Text style={Dashboard.TextoAlerta}>Muestra Protegida</Text>
         </View>
-        <View style={Dashboard.caja_3}>
+        <View style={Dashboard.caja}>
             <Text>contenedor #id</Text>
             <Text style={Dashboard.TextoAlerta}>Precaucion</Text>
         </View>
