8843c651a1f772c9a15cff8de4a61a3d66666dfb
IngRamirezJezhuah <ramirezalvaradodavidjezhuah@gmail.com>
2025-06-24 12:41:50 -0600

    acomoda-el-escaner

---
+++ FRONTEND/Movil/integradora/screens/Escaner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View } from 'react-native'
+
+const Escaner = () => {
+    return (
+        <View>
+            <Text>Escaner</Text>
+        </View>
+    )
+}
+
+export default Escaner
