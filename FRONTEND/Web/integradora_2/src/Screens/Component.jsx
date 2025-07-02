2d584888c6bbd7258075900acf42a6fa7c8dc650
IngRamirezJezhuah <ramirezalvaradodavidjezhuah@gmail.com>
2025-07-02 00:02:01 -0600

    acomodando

--- FRONTEND/Web/integradora_2/src/Screens/Component.jsx
+++ FRONTEND/Web/integradora_2/src/Screens/Component.jsx
@@ -1,42 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Outlet, Link, useLocation } from 'react-router-dom'
 import Dashboard from './Dashboard';
 //import styles from './Component.module.css';
 
 const Component=() => {
     const location = useLocation();
-
+    const [showMenu,setShowMenu] = useState(true);
     const isActive = (path) => location.pathname === path;
+
+    const HandleMenuDinamico = () => setShowMenu(!showMenu) ;
     return (
         <section className='contenedor' >
             <div className='tabNavigator'>
                 <div>
-                    <img className='menu' src="/menu.png" alt='icon-menu' />
-                    <Link to="/Dashboard">
-                        <li className={`bordes ${isActive('/Dashboard') ? 'active' : ''}`}><img src="/dash.png" alt="" /></li>
-                        <li>Dashboard</li>
-                    </Link>
-                    <Link to="/Pacientes">
-                        <li className={`bordes ${isActive('/Pacientes') ? 'active' : ''}`}><img src="/usuario.png" alt="" /></li>
-                        <li>Pacientes</li>
-                    </Link>
-                    <Link to="/Pedidos">
-                        <li className={`bordes ${isActive('/Pedidos') ? 'active' : ''}`}><img src="/pedido.png" alt="" /></li>
-                        <li>Pedido</li>
-                    </Link>
-                    <Link to="/Muestras">
-                        <li className={`bordes ${isActive('/Muestras') ? 'active' : ''}`}><img src="/muestras.png" alt="" /></li>
-                        <li>Muestras</li>
-                    </Link>
-                    <Link to="/Analisis">
-                        <li className={`bordes ${isActive('/Analisis') ? 'active' : ''}`}><img src="/analisis.png" alt="" /></li>
-                        <li>analisis</li>
-                    </Link>
-                    <Link to='/login'>
-                        <div className='cerrar-sesion'>
-                            <li className="bordes"><img src="/salida.png" alt="" /></li>
-                        </div>
-                    </Link>
+                    <img className='menu' src="/menu.png" alt='icon-menu' onClick={HandleMenuDinamico}/>
+                    {showMenu && (
+                    <div className='sidebar'>
+                        <Link to="/Dashboard">
+                            <li className={`bordes ${isActive('/Dashboard') ? 'active' : ''}`}><img src="/dash.png" alt="" /></li>
+                            <li className='textbarra'>Dashboard</li>
+                        </Link>
+                        <Link to="/Pacientes">
+                            <li className={`bordes ${isActive('/Pacientes') ? 'active' : ''}`}><img src="/usuario.png" alt="" /></li>
+                        </Link>
+                            <li>Pacientes</li>
+                        <Link to="/Pedidos">
+                            <li className={`bordes ${isActive('/Pedidos') ? 'active' : ''}`}><img src="/pedido.png" alt="" /></li>
+                        </Link>
+                            <li>Pedido</li>
+                        <Link to="/Muestras">
+                            <li className={`bordes ${isActive('/Muestras') ? 'active' : ''}`}><img src="/muestras.png" alt="" /></li>
+                        </Link>
+                            <li>Muestras</li>
+                        <Link to="/Analisis">
+                            <li className={`bordes ${isActive('/Analisis') ? 'active' : ''}`}><img src="/analisis.png" alt="" /></li>
+                        </Link>
+                            <li>analisis</li>
+                        <Link to='/login'>
+                            <div className='cerrar-sesion'>
+                                <li className="bordes"><img src="/salida.png" alt="" /></li>
+                            </div>
+                        </Link>
+                    </div>
+                    )}
+                    
                 </div>
                 {/* <div className='plantilla'>
                     <Outlet /> 
