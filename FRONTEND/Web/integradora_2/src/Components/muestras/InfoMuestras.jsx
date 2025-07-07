c24679c6e398a2437788367612a5e07fe8545cfc
IngRamirezJezhuah <ramirezalvaradodavidjezhuah@gmail.com>
2025-07-06 18:19:43 -0600

    borrando dependencias rotas

---
+++ FRONTEND/Web/integradora_2/src/Components/muestras/InfoMuestras.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const InfoMuestras = () => {
+    return (
+        <div className='moda-overlay'>
+            <div className='scale-on-hor-center'>
+                <div className='modla'>
+                    <h2>info de muestras</h2>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default InfoMuestras
